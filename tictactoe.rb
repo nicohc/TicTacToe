718517f3f2df4a685474e9f8342675e2e40de859
Nico <nico1099@hotmail.fr>
2018-01-26 05:34:17 +0100

    Morpion fonctionnel

--- tictactoe.rb
+++ tictactoe.rb
@@ -25,10 +25,33 @@ class Board
         print "\n"
     end
 
-    def remplircase(casejouee, form)
+    def remplircase(casejouee, sign)
       #La fonction va remplir la case [n-1] correspondant à la case n du tableau
       #La case sera remplie de la forme du joueur ayant invoqué la fonction
-      @cases[casejouee-1] = form.to_s
+      @cases[casejouee-1] = sign.to_s
+    end
+
+
+    def win_cases
+        if (@cases[0] == @cases[3] && @cases[3] == @cases[6])== ($player1.sign || $player2.sign)
+          return true
+        elsif (@cases[1] == @cases[4] && @cases[4] == @cases[7])== ($player1.sign || $player2.sign)
+          return true
+        elsif (@cases[2] == @cases[5] && @cases[5] == @cases[8])== ($player1.sign || $player2.sign)
+          return true
+        elsif (@cases[0] == @cases[1] && @cases[1] == @cases[2])== ($player1.sign || $player2.sign)
+          return true
+        elsif (@cases[3] == @cases[4] && @cases[4] == @cases[5])== ($player1.sign || $player2.sign)
+          return true
+        elsif (@cases[6] == @cases[7] && @cases[7] == @cases[8])== ($player1.sign || $player2.sign)
+          return true
+        elsif (@cases[0] == @cases[4] && @cases[4] == @cases[8])== ($player1.sign || $player2.sign)
+          return true
+        elsif (@cases[2] == @cases[4] && @cases[4] == @cases[6])== ($player1.sign || $player2.sign)
+          return true
+        else
+          return false
+        end
     end
 
 end
@@ -50,23 +73,11 @@ end
 # -------- Player ----------------------------------#
 class Player
 
-  attr_accessor :player1 , :player2 , :player1status, :player2status, :casespossedees1 , :casespossedees2
+  attr_accessor :playername , :sign
 
-  def initialize
-    p "What's the name of the first player ?"
-    $player1 = gets.chomp   #On insere le nom du premier joueur
-    @form = "X"             #On détermine la forme qui le représente
-    p "You are the 'X' ! "
-    $player1winner = false
-    @casespossedees1 = []
-    print "\n"
-
-    p "What's the name of the second player ?"
-    $player2 = gets.chomp
-    @form = "O"
-    p "You are the 'O' ! "
-    @casespossedees2 = []
-    $player2winner = false
+  def initialize(playername, sign)
+    @playername = playername
+    @sign = sign
   end
 
 end
@@ -78,31 +89,44 @@ class Game
   attr_accessor :casejouee
 
   def initialize
-    p "Hello, let's begin !"
+    p "Commencons la partie!"
+    p "Quel est le nom du premier joueur ?"
+    playername1 = gets.chomp.to_s
+    p "Et son signe ?"
+    sign1 = gets.chomp
 
-    #On appelle la création des joueurs
-    Player.new
-    p "The players are : #{$player1} and #{$player2}."
-    @newboard = Board.new
+    #On appelle la création du joueur1
+    $player1 = Player.new(playername1, sign1)
+
+    p "Quel est le nom du second joueur ?"
+    playername2 = gets.chomp
+    p "Et son signe ?"
+    sign2 = gets.chomp.to_s
+
+    #On appelle la création du joueur1
+    $player2 = Player.new(playername2, sign2)
+
+    p "Les joueurs sont: 'playername1' and 'playername2'."
+    @newboard = Board.new  #On initialise un nouveau plateau de jeu
     @tour = 1
-    @casedejajouee = []
-    nouveautour
+    @casedejajouee = []   #On liste ici les cases déjà remplies
+    nouveautour           #On lance le premier tour
   end
 
   def nouveautour
-      unless ($player1winner || $player2status)
+      unless @newboard.win_cases || @tour > 10   #A moins qu'il y ait un vainqueur ou que l'on ait fini de remplir le tableau , on lance un nouveau tour
         if @tour%2 == 1
-            p "Player's turn : #{$player1}"
-            p "Select one case"
+            p "Au tour de : #{$player1.playername}"
+            p "Selectionner une case"
             casejouee = gets.chomp
             if casejouee.match(/[1-9]/)  # Regexp pour limiter les valeurs possibles aux chiffres entre 0 et 9
 
-              if @casedejajouee.include?(casejouee)
+              if @casedejajouee.include?(casejouee) #Si la case est déjà jouée , cad répértoriée dans la liste des "casedejajouee", alprs on recommence.
                 p "Case déjà jouée"
                 nouveautour
               else
-                @casedejajouee << casejouee
-                @newboard.remplircase(casejouee.to_i,"X")
+                @casedejajouee << casejouee #A chaque fois qu'une nouvelle case est jouée, elle est stockée dans la liste casedejajouee
+                @newboard.remplircase(casejouee.to_i,$player1.sign)
                 @newboard.boardgame
                 @tour +=1
                 nouveautour
@@ -114,8 +138,8 @@ class Game
             end # Fin test matching 1-9
 
         else
-            p "Player's turn : #{$player2}"
-            p "Select one case"
+            p "Au tour de : #{$player2.playername}"
+            p "Selectionner une case"
             casejouee = gets.chomp
             if casejouee.match(/[1-9]/)  # Regexp pour limiter les valeurs possibles aux chiffres entre 0 et 9
 
@@ -124,7 +148,7 @@ class Game
                   nouveautour
                 else
                   @casedejajouee << casejouee
-                  @newboard.remplircase(casejouee.to_i,"O")
+                  @newboard.remplircase(casejouee.to_i,$player2.sign)
                   @newboard.boardgame
                   @tour +=1
                   nouveautour
@@ -136,10 +160,13 @@ class Game
             end # Fin test matching 1-9
 
         end # Fin Tour
+      else
+        p "Fin de partie"
       end #Fin unless
+
   end #Fin nouveautour
 
-end   # Fin Class Game
+end # Fin Class Game
 
 # ------------------------------  Lancement  ----------------------------------#
 
